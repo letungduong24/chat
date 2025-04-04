ab082d2bc5ae60306dc74923416323f2b841c874
ldngduong <letungduong1624@gmail.com>
2025-04-05 01:33:07 +0700

    fix cors

--- back/index.js
+++ back/index.js
@@ -19,7 +19,6 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(express.json());
 app.use(cors({
     origin: process.env.ORIGIN_URL,
     credentials: true
@@ -29,6 +28,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
 
 server.listen(port, () => {
+    console.log(process.env.ORIGIN_URL)
     console.log('server is running')
     connectDB()
 })
